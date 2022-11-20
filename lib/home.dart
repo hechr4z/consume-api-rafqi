3833761388b47c5d98296f5112c98352b9a66050
hechr4z <kenb.space3112@gmail.com>
2022-11-20 22:35:10 +0700

    first commit

--- lib/home.dart
+++ lib/home.dart
@@ -4,57 +4,85 @@ import 'package:newsapi/detail.dart';
 import 'package:http/http.dart' as http;
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
-  _HomeState createState() => _HomeState();
+  State<Home> createState() => _HomeState();
 }
 
 class _HomeState extends State<Home> {
-  List _posts = [];
-
+  List _post = [];
   @override
   void initState() {
     super.initState();
     _getData();
   }
 
-  @override
+  Future _getData() async {
+    final String api =
+        "https://newsapi.org/v2/everything?q=valorant&from=2022-10-20&sortBy=publishedAt&apiKey=cf00ce1b056f42b382e1ecfb7a7a8701";
+    try {
+      final response = await http.get(Uri.parse(api));
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+        setState(() {
+          _post = data['articles'];
+        });
+        print(_post);
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Flutter Http')),
+      appBar: AppBar(
+        title: Text('RESTFUL API'),
+      ),
       body: ListView.builder(
-        itemCount: _posts.length,
+        itemCount: _post.length,
         itemBuilder: (context, index) {
           return ListTile(
             leading: Container(
               color: Colors.grey[200],
               height: 100,
               width: 100,
+              child: _post[index]['urlToImage'] != null
+                  ? Image.network(
+                      _post[index]['urlToImage'],
+                      width: 100,
+                      fit: BoxFit.cover,
+                    )
+                  : Center(),
             ),
-            title: Text('${_posts[index]['title']}'),
-            subtitle: Text('${_posts[index]['description']}'),
+            title: Text(
+              '${_post[index]['title']}',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
+            subtitle: Text(
+              '${_post[index]['description']}',
+              maxLines: 2,
+              overflow: TextOverflow.ellipsis,
+            ),
             onTap: () {
               Navigator.push(
-                  context, MaterialPageRoute(builder: (c) => Detail()));
+                context,
+                MaterialPageRoute(
+                  builder: (context) => Detail(
+                      Url: _post[index]['url'],
+                      title: _post[index]['title'],
+                      content: _post[index]['content'],
+                      publishedAt: _post[index]['publishedAt'],
+                      author: _post[index]['author'],
+                      urlToImage: _post[index]['urlToImage']),
+                ),
+              );
             },
           );
         },
       ),
     );
-  }
-
-  Future _getData() async {
-    try {
-      final response = await http.get(Uri.parse(
-          'https://newsapi.org/v2/everything?q=valorant&from=2022-10-20&sortBy=publishedAt&apiKey=cf00ce1b056f42b382e1ecfb7a7a8701'));
-
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        setState(() {
-          _posts = data['articles'];
-        });
-      }
-    } catch (e) {
-      print(e);
-    }
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:newsapi/home.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  runApp(const MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       title: 'Flutter Http',
-      theme: ThemeData(appBarTheme: AppBarTheme(elevation: 0)),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
       home: Home(),
     );
   }
